46cd76b76a695e2413cdd6a4c18300348b9b7203
serlarr <serlarr@gmail.com>
2021-04-18 11:53:48 +0200

    aniadido un parrafo

---
+++ arrays.js
@@ -0,0 +1,19 @@
+//  let frutas = ["pera", "manzana", "platano"];
+//  document.write(frutas, frutas[0]);
+
+//arrays asociativos. 
+let persona = {
+    nombre : "sergio",
+    apellido : "larrea"
+};
+
+document.write(persona["nombre"]);
+
+let nombre = persona["nombre"];
+let apellido = apellido["apellido"];
+//no se muestra no sé por qué
+frase = `Mi nombre es: ${nombre}<br>
+        y mi apellido: ${apellido}`;
+
+document.write(frase);
+
